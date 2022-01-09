a24cf346b2302209f97abbdda8098e4b09088a08
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-09 23:19:07 +0530

    Added more files

---
+++ my-app/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next JS 2</title>
+      </Head>
+      <h1>ABOUT</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ my-app/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NEXT JS 1</title>
+        <meta name="keywords" content="webd, programming" />
+      </Head>
+
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
